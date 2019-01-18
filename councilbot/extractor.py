5d61d665283ba538d41f622058b106d51d3f65ac
Jonas Schäfer <j.wielicki@sotecware.net>
2019-01-18 17:07:23 +0100

    extractor: implement github xeps PR extractor
    Fixes #2.

--- councilbot/extractor.py
+++ councilbot/extractor.py
@@ -13,15 +13,17 @@ PROTOXEP_URL_RE = re.compile(
 PROTOXEP_URL_TEMPLATE = "https://xmpp.org/extensions/inbox/{basename}.xml"
 PROTOXEP_HTML_URL_TEMPLATE = "https://xmpp.org/extensions/inbox/{basename}.html"
 
-XEPS_PR_URL = re.compile(
+XEPS_PR_URL_RE = re.compile(
     r"https?://(www\.)?github\.com/xsf/xeps/pull/(?P<num>[0-9]+)\S*",
     re.I,
 )
 XEPS_PR_URL_TEMPLATE = "https://api.github.com/repos/xsf/xeps/pulls/{num}"
+XEPS_PR_HTML_URL_TEMPLATE = "https://github.com/xsf/xeps/pull/{num}"
 XEPS_PR_FILES_URL_TEMPLATE = \
     "https://api.github.com/repos/xsf/xeps/pulls/{num}/files"
 MAX_READ_SIZE = 10*1024*1024  # 10 MiB -- more than enough for all current XEPs
 READ_CHUNK_SIZE = 4096
+XEP_FILE_RE = re.compile(r"(xep-[0-9]{4}).xml")
 
 BAD_SHORT_NAME_RE = re.compile(
     "^not[\W_]yet[\W_]assigned|None|N/A$",
@@ -82,8 +84,59 @@ async def _extract_protoxep_metadata(match):
     )
 
 
+async def _extract_xeps_pr_metadata(match):
+    matched_url = match.group(0)
+    num = match.groupdict()["num"]
+    url = XEPS_PR_URL_TEMPLATE.format(num=num)
+    files_url = XEPS_PR_FILES_URL_TEMPLATE.format(num=num)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            pr_json = await response.json()
+
+        files_json = []
+        # using the xep name in the tag makes it too long for fuzzy match (even
+        # without PR prefix)
+        # async with session.get(files_url) as response:
+        #     files_json = await response.json()
+
+    title = pr_json["title"]
+    # normalize all the spacing
+    description = " ".join(pr_json["body"].split())
+    if len(description) > 300:
+        description = description[:300] + "…"
+
+    tag = "PR#{}".format(num)
+
+    affected_xep = None
+    for file_info in files_json:
+        match = XEP_FILE_RE.match(file_info["filename"])
+        if match is None:
+            continue
+        if affected_xep:
+            affected_xep = None
+            break
+
+        affected_xep = match.group(1)
+
+    # makes it too long for fuzzy match :(
+    # if affected_xep:
+    #     tag = "{} {}".format(tag, affected_xep.upper())
+
+    return URLMetadata(
+        matched_url=matched_url,
+        title="[PR#{}] {}".format(num, title),
+        description=description,
+        tag=tag,
+        urls=[
+            XEPS_PR_HTML_URL_TEMPLATE.format(num=num)
+        ]
+    )
+
+
 _IMPLEMENTATIONS = [
-    (PROTOXEP_URL_RE, _extract_protoxep_metadata)
+    (PROTOXEP_URL_RE, _extract_protoxep_metadata),
+    (XEPS_PR_URL_RE, _extract_xeps_pr_metadata),
 ]
 
 
@@ -93,6 +146,6 @@ async def extract_url_metadata(url):
         if match is None:
             continue
 
-        return (await _extract_protoxep_metadata(match))
+        return (await extractor(match))
 
     return None
